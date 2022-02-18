6cfc20f48f9091fc0d8caca62540bd00b357fda4
AlexSeven77 <alex901109zc@icloud.com>
2022-02-18 17:44:14 +0800

    项目中放入jQuery

--- 04-JavaScript基础/test/19.js
+++ 04-JavaScript基础/test/19.js
@@ -53,14 +53,23 @@
 
 var arr = [1, 2, 3, 2, 2, 1, 3, 4, 2, 5];
 
-for (var i = 0; i<arr.length;i++){
-    for (var j =i+1; j<arr.length;j++){
-        if (arr[i]==arr[j]){
-            arr.splice(j,1)
-            j--;
-        }
+// for (var i = 0; i<arr.length;i++){
+//     for (var j =i+1; j<arr.length;j++){
+//         if (arr[i]==arr[j]){
+//             arr.splice(j,1)
+//             j--;
+//         }
         
-    }
+//     }
+// }
+
+arr.forEach((item,index)=>{
+    item=item+1
+})
+
+for (const [index, item] of arr.entries()){
+    console.log([index,item])
 }
-console.log(arr)
-console.log(arr.reverse())
+
+// console.log(arr)
+// console.log(arr.reverse())
